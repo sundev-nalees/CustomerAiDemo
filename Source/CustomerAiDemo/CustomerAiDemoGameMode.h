f387331b62dbf2680354d74f0bb8a250d351cc7f
Sundev Nalees <72244488+sundev-nalees@users.noreply.github.com>
2025-03-21 15:55:43 +0530

    Initial commit

---
+++ Source/CustomerAiDemo/CustomerAiDemoGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CustomerAiDemoGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ACustomerAiDemoGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ACustomerAiDemoGameMode();
+};
+
+
+
